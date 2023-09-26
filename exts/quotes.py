f1405c0691ec151efefaa71aa984be6f816d3270
PythiaUF <125420118+PythiaUF@users.noreply.github.com>
2023-09-26 18:13:09 -0400

    docs: comment random quote example

--- exts/quotes.py
+++ exts/quotes.py
@@ -6,7 +6,7 @@ import interactions as ipy
 class Quotes(ipy.Extension):
     @ipy.slash_command()
     async def random_quote(self, ctx: ipy.SlashContext):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"{os.environ['API_URL']}/quote") as resp:
-                data = await resp.json()
-                await ctx.send(f"> {data['quote']}\n\\- {data['author']}")
+        async with aiohttp.ClientSession() as session:  # create session to make web request
+            async with session.get(f"{os.environ['API_URL']}/quote") as resp:  # make web request to api website's quotes
+                data = await resp.json()  # get the data from the response - it's a dictionary {"quote": "e", "author": "e"}
+                await ctx.send(f"> {data['quote']}\n\\- {data['author']}")  # send it as a message
